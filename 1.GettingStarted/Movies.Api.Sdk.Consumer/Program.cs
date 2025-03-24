bf8b10b3d5c2601a34ae395fb5e7ab492f26df03
Árthur Tolfo Pinheiro <arthur.pinheiro@elsys.com.br>
2025-03-24 07:51:39 -0300

    Using refit http factory

--- 1.GettingStarted/Movies.Api.Sdk.Consumer/Program.cs
+++ 1.GettingStarted/Movies.Api.Sdk.Consumer/Program.cs
@@ -1,11 +1,19 @@
-﻿using Movies.Api.Sdk;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Movies.Api.Sdk;
 using Refit;
 using System.Text.Json;
 
-var moviesApi = RestService.For<IMoviesApi>("https://localhost:5001");
-var movie = await moviesApi.GetMovieAsync("eecb7cb0-d570-4112-9b2f-a0e67a21204a");
+//var moviesApi = RestService.For<IMoviesApi>("https://localhost:5001");
+//var movie = await moviesApi.GetMovieAsync("eecb7cb0-d570-4112-9b2f-a0e67a21204a");
+//Console.WriteLine(JsonSerializer.Serialize(movie));
 
-Console.WriteLine(JsonSerializer.Serialize(movie));
+var services = new ServiceCollection();
+
+services.AddRefitClient<IMoviesApi>()
+    .ConfigureHttpClient(x => 
+        x.BaseAddress = new Uri("https://localhost:5001"));
+var provider = services.BuildServiceProvider();
+var moviesApi = provider.GetRequiredService<IMoviesApi>();
 
 var movies = await moviesApi.GetMoviesAsync(new Movies.Contracts.Requests.V1.GetAllMoviesRequest 
 { 
